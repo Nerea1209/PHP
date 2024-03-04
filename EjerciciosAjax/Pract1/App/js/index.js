2426ff21835187dd3a709c9c0762b2337856a14a
Nerea1209 <nlopsan0104@g.educaand.es>
2024-03-04 10:03:16 +0100

    Termina funcionalidad de productos+

--- EjerciciosAjax/Pract1/App/js/index.js
+++ EjerciciosAjax/Pract1/App/js/index.js
@@ -18,7 +18,7 @@ function obtener_productos() {
             }
             else {
                 var tabla_productos = "<table>";
-                tabla_productos += "<tr><th>COD</th><th>Nombre Corto</th><th>PVP</th><th><form action='index.html' method='post'> <button class='enlace' type='submit' name='btnInsertar' onclick='form_insertar()'>Productos+</button></form></th></tr>";
+                tabla_productos += "<tr><th>COD</th><th>Nombre Corto</th><th>PVP</th><th><form onsubmit='event.preventDefault();'> <button class='enlace' type='submit' name='btnInsertar' onclick='form_insertar()'>Productos+</button></form></th></tr>";
                 $.each(data.productos, function (key, tupla) {
                     tabla_productos += "<tr><form action='index.php' method='post'>";
                     tabla_productos += "<td><button class='enlace' type='submit' name='btnDetalles' onclick='obtener_producto(\"" + tupla["cod"] + "\")' value='" + tupla["cod"] + "'>" + tupla["cod"] + "</button></td>";
@@ -122,72 +122,15 @@ function form_insertar() {
                 form += "<form onsubmit='event.preventDefault(); '>";
                 form += "<p><label for='cod'>Código: </label> <input type='text' name='cod' id='cod' maxlength='12'></p>";
                 form += '<p><label for="nombre">Nombre: </label><input type="text" name="nombre" id="nombre" maxlength="200"></p>';
-
-                // <p>
-                //     <label for="nombre">Nombre: </label>
-                //     <input type="text" name="nombre" id="nombre" maxlength="200" value="<?php if (isset($_POST["nombre"])) echo $_POST["nombre"] ?>">
-                // </p>
-                // <p>
-                //     <label for="nombre_corto">Nombre corto: </label>
-                //     <input type="text" name="nombre_corto" id="nombre_corto" maxlength="50" value="<?php if (isset($_POST["nombre_corto"])) echo $_POST["nombre_corto"] ?>">
-                //     <?php
-                //     if (isset($_POST["nombre_corto"]) && $error_nombre_corto)
-                //         echo "<span class='error'> Campo vacío</span>";
-                //     ?>
-                // </p>
-                // <p>
-                //     <label for="descripcion">Descripción: </label>
-                //     <textarea name="descripcion" id="descripcion"><?php if (isset($_POST["descripcion"])) echo $_POST["descripcion"] ?></textarea>
-                // </p>
-                // <p>
-                //     <label for="pvp">PVP: </label>
-                //     <input type="text" name="pvp" id="pvp" maxlength="11" value="<?php if (isset($_POST["pvp"])) echo $_POST["pvp"] ?>">
-                //     <?php
-                //     if (isset($_POST["pvp"]) && $error_pvp) {
-                //         if ($_POST["pvp"] == "")
-                //             echo "<span class='error'> Campo vacío</span>";
-                //         else if (!is_numeric($_POST["pvp"]))
-                //             echo "<span class='error'> No es un número</span>";
-                //         else
-                //             echo "<span class='error'> Tiene que ser un valor positivo</span>";
-                //     }
-                //     ?>
-                // </p>
-                // <p>
-                //     <label for="familia">Seleccione una familia: </label>
-                //     <select name="familia" id="familia">
-                //         <?php
-                //         // Obtenemos las familias
-                //         $url = DIR_SERV . "/familias";
-                //         $respuesta = consumir_servicios_REST($url, "GET");
-                //         $obj = json_decode($respuesta);
-                //         if (!$obj) {
-                //             session_destroy();
-                //             die("<p>Error consumiendo el servicio: " . $url . "</p>" . $respuesta);
-                //         }
-
-                //         if (isset($obj->error)) {
-                //             session_destroy();
-                //             die("<p>" . $obj->error . "</select></p></body></html>");
-                //         }
-
-                //         foreach ($obj->familias as $tupla) {
-                //             if (isset($_POST["familia"]) && $tupla->cod == $_POST["familia"])
-                //                 echo "<option selected value='" . $tupla->cod . "'>$tupla->nombre</option>";
-                //             else
-                //                 echo "<option value='" . $tupla->cod . "'>$tupla->nombre</option>";
-                //         }
-                //         ?>
-                //     </select>
-                // </p>
-                // <p>
-                //     <button type="submit" name="btnVolver">Volver</button>
-                //     <button type="submit" name="btnContInsertar">Continuar</button>
-                // </p>
-
+                form += '<p><label for="descripcion">Descripción: </label><textarea name="descripcion" id="descripcion"></textarea></p>';
+                form += '<p><label for="familia">Seleccione una familia: </label><select name="familia" id="familia">'
+                $.each(data.familias, function (key, tupla) {
+                    form += "<option value='" + tupla["cod"] + "'>" + tupla["nombre"] + "</option>";
+                })
+                form += '</select></p>';
+                form += '<p><button type="submit" name="btnVolver" onclick="volver()">Volver</button><button type="submit" name="btnContInsertar">Continuar</button></p>';
                 $('#errores').html("");
-                $('#respuesta').html("");
-                $('#tabla').html(tabla_productos);
+                $('#respuesta').html(form);
             }
         })
 
